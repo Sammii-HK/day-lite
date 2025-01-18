be8ab4010e4ac47558aa2a47641ca642e6f464db
Sammii <kellow.sammii@gmail.com>
2025-01-18 14:17:17 +0000

    refactoring logic

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import { Observer, SearchRiseSet, Body } from 'astronomy-engine';
 import dayjs from 'dayjs';
 import mapboxgl from 'mapbox-gl'
 import utc from 'dayjs/plugin/utc';
+import isBetween from 'dayjs/plugin/isBetween';
 
 function App() {
   const mapRef = useRef<mapboxgl.Map>()
   const mapContainerRef = useRef<HTMLDivElement | null>(null)
 
   dayjs.extend(utc);
+  dayjs.extend(isBetween);
   const day = dayjs().toDate();
 
   const greenwichLongLat = { lat: 51.4769, long: 0.0005};
@@ -26,13 +28,12 @@ function App() {
 
   const [observerLocation, setObserverLocation] = useState(greenwichLongLat);
 
-  const sunrise  = SearchRiseSet(Body.Sun, observer({lat: observerLocation.lat, long: observerLocation.long}), Direction.Rise, day, 1);
+  const currentUtcOffset = calculateUtcOffset(observerLocation.long);
+  const getSunrise = (days: number) => SearchRiseSet(Body.Sun, observer({lat: observerLocation.lat, long: observerLocation.long}), Direction.Rise, day, days);
+
+  const sunrise  = getSunrise(1);
   const sunset  = SearchRiseSet(Body.Sun, observer({lat: observerLocation.lat, long: observerLocation.long}), Direction.Set, day, 1);
-  const daylightMinutes = dayjs(sunrise?.date).diff(dayjs(sunset?.date), 'minute');
-  const daylightHours = Math.floor(daylightMinutes / 60);
-  const isInPast = daylightMinutes < 0;
-  const getMinutes = daylightMinutes - (60 * daylightHours)
-  
+  const lastSunrise = dayjs(sunrise?.date).get('date') > dayjs(sunset?.date).get('date') ? getSunrise(-1) : sunrise;
 
   const multipleHours = (hours: number) => {
     return hours > 1 ? 's' : '';
@@ -43,13 +44,15 @@ function App() {
   
     return Math.max(-12, Math.min(14, offset));
   }
-
-  const currentUtcOffset = calculateUtcOffset(observerLocation.long);
   
-  const daylightTime = `${Math.abs(daylightHours)} hour${multipleHours(daylightHours)} ${getMinutes} minute${multipleHours(getMinutes)}`;
-  const sunriseTime = dayjs(sunrise?.date).utcOffset(currentUtcOffset).format('HH:mm');
+  const sunriseTime = lastSunrise ? dayjs(lastSunrise.date).utcOffset(currentUtcOffset).format('HH:mm') : '';
   const sunsetTime = dayjs(sunset?.date).utcOffset(currentUtcOffset).format('HH:mm');
-  
+  const daylightMinutes = dayjs(lastSunrise?.date).diff(dayjs(sunset?.date), 'minute');
+  const getMinutes = Math.abs(daylightMinutes % 60);
+  const daylightHours = Math.floor(daylightMinutes / 60);
+  const daylightTime = `${Math.abs(daylightHours)} hour${multipleHours(daylightHours)} ${getMinutes} minute${multipleHours(getMinutes)}`;
+  const isDayTime = dayjs().isBetween(dayjs(lastSunrise?.date), dayjs(sunset?.date));
+
   useEffect(() => {    
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_KEY;
     mapRef.current = new mapboxgl.Map({
@@ -75,7 +78,7 @@ function App() {
         <h1 id='title' className='sm mt-0'>Day Lite</h1>
       <div id="info-container">
         <h2>Daylight hours: {daylightTime}</h2>
-        <div>{isInPast ? "🌘" : "🌤️"} Sunrise: {sunriseTime} Sunset: {sunsetTime}</div>
+        <div>{!isDayTime ? "🌘" : "🌤️"} Sunrise: {sunriseTime} Sunset: {sunsetTime}</div>
       </div>
       <div id='map-container' ref={mapContainerRef}/>
     </>
